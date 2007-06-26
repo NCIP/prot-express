1cc27d7a2d2f03d3ca43a27b6afcf2de23f96276
kanchink <kanchink>
2007-06-26 12:54:55 +0000

    SVN-Revision: 80

---
+++ docs/analysis_and_design/models/html_docs/domain_model/tocTab.js
@@ -0,0 +1,11 @@
+var tocTab = new Array();
+tocTab[0] = new Array ("0", "Domain Model", "EARoot/contents.htm", "");
+tocTab[1] = new Array ("1", "gov.nih.nci.protexpress.domain", "EARoot/EA1/contents.htm", "");
+tocTab[2] = new Array ("1.1", "audit", "EARoot/EA1/EA1/contents.htm", "");
+tocTab[3] = new Array ("1.2", "contact", "EARoot/EA1/EA2/contents.htm", "");
+tocTab[4] = new Array ("1.3", "experiment", "EARoot/EA1/EA3/contents.htm", "");
+tocTab[5] = new Array ("1.4", "ontology", "EARoot/EA1/EA4/contents.htm", "");
+tocTab[6] = new Array ("1.5", "protocol", "EARoot/EA1/EA5/contents.htm", "");
+tocTab[7] = new Array ("1.6", "util", "EARoot/EA1/EA6/contents.htm", "");
+tocTab[8] = new Array ("2", "java.lang", "EARoot/EA2/contents.htm", "");
+var nCols = 20;
