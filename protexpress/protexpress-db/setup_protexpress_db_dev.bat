ec62b5071ef596480588b9a947eaf654534b3fb8
kanchink <kanchink>
2007-07-30 06:28:49 +0000

    SVN-Revision: 99

---
+++ protexpress/protexpress-db/setup_protexpress_db_dev.bat
@@ -0,0 +1,1 @@
+CALL setup_protexpress_database.bat cbiodb590.nci.nih.gov 5457 protexpress gpsxar protexpress_ddl.sql out.txt
---
+++ protexpress/protexpress-db/setup_protexpress_db_localhost.bat
@@ -0,0 +1,1 @@
+CALL setup_protexpress_database.bat localhost 5432 protexpress gpsxar protexpress_ddl.sql out.txt
