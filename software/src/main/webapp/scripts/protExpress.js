ddf6542346aea0858eae00b3fa0fcbb217b94c9d
smiller <smiller>
2007-09-14 22:00:32 +0000

    improved menu highlighting
    SVN-Revision: 256

---
+++ software/src/main/webapp/scripts/protExpress.js
@@ -0,0 +1,37 @@
+function getCurrentSection(hrefString, appContext) {
+    var arr = hrefString.split('/');
+
+    for(var i = 0; i < arr.length; i++) {
+        if (arr[i].length > 0 && ('/' + arr[i].toLowerCase()) != appContext.toLowerCase()) {
+            return arr[i].toLowerCase();
+        }
+    }
+    return null;
+}
+
+function setActiveMenu(appContext) {
+    if(document.location.href) {
+        currentLocation = document.location.href;
+    } else {
+        currentLocation = document.location;
+    }
+
+    if (document.getElementById('leftnav')!= null && currentLocation != null) {
+        menuItems = document.getElementById('leftnav').getElementsByTagName('a');
+        for(var i = 0; i < menuItems.length; i++) {
+            if(menuItems[i].href == currentLocation) {
+                menuItems[i].className = 'selected';
+                return;
+            }
+        }
+        if (document.location.pathname != null && getCurrentSection(document.location.pathname, appContext) != null) {
+            currentSection = getCurrentSection(document.location.pathname, appContext);
+            for(var i = 1; i < menuItems.length; i++) {
+                if(getCurrentSection(menuItems[i].pathname, appContext) == currentSection) {
+                    menuItems[i].className = 'selected';
+                    return;
+                }
+            }
+        }
+    }
+}
