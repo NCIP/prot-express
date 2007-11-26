d0567cae933cd8a7b16ac36f6887d9ea9a33133d
klingerc <klingerc@mail.nih.gov>
2007-11-26 05:02:55 +0000

    SVN-Revision: 348

--- src/main/webapp/help/wwhdata/js/search/pairs/pair4.js
+++
@@ -1,9 +0,0 @@
-function FileData_Pairs(x)
-{
-x.t("protocols","managing");
-x.t("protocols","documentation");
-x.t("protocols","using");
-x.t("managing","protocols");
-x.t("protexpress","managing");
-x.t("using","protexpress");
-}
