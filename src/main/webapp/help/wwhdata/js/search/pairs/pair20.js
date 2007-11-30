ec01490877182b8238c2faf8e89fd05e8b849392
klingerc <klingerc@mail.nih.gov>
2007-11-30 04:06:35 +0000

    SVN-Revision: 351

---
+++ src/main/webapp/help/wwhdata/js/search/pairs/pair20.js
@@ -0,0 +1,9 @@
+function FileData_Pairs(x)
+{
+x.t("protexpress","importing");
+x.t("importing","data");
+x.t("data","importing");
+x.t("data","documentation");
+x.t("data","using");
+x.t("using","protexpress");
+}
---
+++ src/main/webapp/help/wwhdata/js/search/pairs/pair21.js
@@ -0,0 +1,9 @@
+function FileData_Pairs(x)
+{
+x.t("protexpress","exporting");
+x.t("exporting","data");
+x.t("data","documentation");
+x.t("data","exporting");
+x.t("data","using");
+x.t("using","protexpress");
+}
