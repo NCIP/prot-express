b9cb576ac95727043c92d8713fdeb632f5f6d951
klingerc <klingerc@mail.nih.gov>
2007-12-05 04:43:18 +0000

    SVN-Revision: 357

---
+++ src/main/webapp/help/wwhdata/js/search/pairs/pair17.js
@@ -0,0 +1,11 @@
+function FileData_Pairs(x)
+{
+x.t("managing","experiments");
+x.t("deleting","experiment");
+x.t("experiments","deleting");
+x.t("working","protexpress");
+x.t("protexpress","managing");
+x.t("experiment","deleting");
+x.t("experiment","working");
+x.t("experiment","documentation");
+}
---
+++ src/main/webapp/help/wwhdata/js/search/pairs/pair9.js
@@ -0,0 +1,26 @@
+function FileData_Pairs(x)
+{
+x.t("searching","experiment");
+x.t("managing","experiments");
+x.t("deleting","experiment");
+x.t("experiments","managing");
+x.t("experiments","working");
+x.t("experiments","section");
+x.t("working","protexpress");
+x.t("protexpress","managing");
+x.t("editing","experiment");
+x.t("editing","protocol");
+x.t("includes","following");
+x.t("procedures","searching");
+x.t("section","includes");
+x.t("experiment","deleting");
+x.t("experiment","run");
+x.t("experiment","adding");
+x.t("application","editing");
+x.t("protocol","application");
+x.t("following","procedures");
+x.t("run","editing");
+x.t("run","adding");
+x.t("adding","experiment");
+x.t("adding","protocol");
+}
