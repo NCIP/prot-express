9b6e6496d3a7faaada172354cd1a708028b375d2
klingerc <klingerc@mail.nih.gov>
2007-12-05 04:43:18 +0000

    SVN-Revision: 357

---
+++ software/src/main/webapp/help/wwhdata/js/search/pairs/pair13.js
@@ -0,0 +1,12 @@
+function FileData_Pairs(x)
+{
+x.t("managing","experiments");
+x.t("experiments","editing");
+x.t("working","protexpress");
+x.t("protexpress","managing");
+x.t("editing","experiment");
+x.t("experiment","run");
+x.t("run","working");
+x.t("run","editing");
+x.t("run","documentation");
+}
---
+++ software/src/main/webapp/help/wwhdata/js/search/pairs/pair8.js
@@ -0,0 +1,11 @@
+function FileData_Pairs(x)
+{
+x.t("protocols","deleting");
+x.t("managing","protocols");
+x.t("deleting","protocol");
+x.t("working","protexpress");
+x.t("protexpress","managing");
+x.t("protocol","deleting");
+x.t("protocol","working");
+x.t("protocol","documentation");
+}
