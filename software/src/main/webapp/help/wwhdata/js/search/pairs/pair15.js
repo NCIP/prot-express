983526ca27d3bc6fed5df09c42cb0c2612a63d53
klingerc <klingerc@mail.nih.gov>
2007-12-06 05:26:02 +0000

    SVN-Revision: 361

---
+++ software/src/main/webapp/help/wwhdata/js/search/pairs/pair15.js
@@ -0,0 +1,11 @@
+function FileData_Pairs(x)
+{
+x.t("managing","experiments");
+x.t("experiments","editing");
+x.t("working","protexpress");
+x.t("protexpress","managing");
+x.t("editing","experiment");
+x.t("experiment","working");
+x.t("experiment","editing");
+x.t("experiment","documentation");
+}
---
+++ software/src/main/webapp/help/wwhdata/js/toc.js
@@ -0,0 +1,25 @@
+function  WWHBookData_AddTOCEntries(P)
+{
+var A=P.fN("protExpress Online Help","0");
+var B=A.fN("Using protExpress Online Help","1");
+A=P.fN("Working with protExpress","2");
+B=A.fN("Using the Dashboard","3");
+B=A.fN("Managing Protocols","4");
+var C=B.fN("Searching for a Protocol","5");
+C=B.fN("Adding a Protocol","6");
+C=B.fN("Editing a Protocol","7");
+B=A.fN("Managing Experiments","8");
+C=B.fN("Searching for an Experiment","9");
+C=B.fN("Adding an Experiment","10");
+C=B.fN("Adding an Experiment Run","11");
+C=B.fN("Editing an Experiment Run","12");
+C=B.fN("Adding a Protocol Application","13");
+C=B.fN("Editing a Protocol Application","14");
+C=B.fN("Editing an Experiment","15");
+B=A.fN("Working with Inputs","16");
+B=A.fN("Working with Outputs","17");
+B=A.fN("Importing Data","18");
+B=A.fN("Exporting Data","19");
+A=P.fN("protExpress Glossary","20");
+A=P.fN("Using Application Support","21");
+}
