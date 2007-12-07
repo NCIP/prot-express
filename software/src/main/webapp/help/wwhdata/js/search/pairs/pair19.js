f1e11926520fd899e86dfb16f0330c3da1b73359
klingerc <klingerc@mail.nih.gov>
2007-12-07 04:26:21 +0000

    SVN-Revision: 363

--- software/src/main/webapp/help/wwhdata/js/search/pairs/pair19.js
+++
@@ -1,9 +0,0 @@
-function FileData_Pairs(x)
-{
-x.t("working","protexpress");
-x.t("protexpress","exporting");
-x.t("exporting","data");
-x.t("data","working");
-x.t("data","documentation");
-x.t("data","exporting");
-}
