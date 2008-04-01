f5e40f4d072313ffe68d60601310f3e1802d1e01
kanchink <kanchink>
2008-04-01 15:27:29 +0000

    SVN-Revision: 493

--- software/src/main/sql/sampleExperiments.sql
+++ software/src/main/sql/sampleExperiments.sql
@@ -1,3 +1,3 @@
 insert into experiment
-  (id, name, description, hypothesis, url, creator, creation_date, modification_date) values
-  (nextval('hibernate_sequence'), 'Peroxisome membrane protein analysis, Marcello Marelli et al.', 'We have combined classical subcellular fractionation with large-scale quantitative mass spectrometry to identify proteins that enrich specifically with peroxisomes of Saccharomyces cerevisiae.', 'Some Hypothesis', 'http://www.jcb.org/cgi/content/abstract/167/6/1099', 'user1', current_date, current_date);
+  (id, name, description, hypothesis, url, creator, creation_date, modification_date, date_performed) values
+  (nextval('hibernate_sequence'), 'Peroxisome membrane protein analysis, Marcello Marelli et al.', 'We have combined classical subcellular fractionation with large-scale quantitative mass spectrometry to identify proteins that enrich specifically with peroxisomes of Saccharomyces cerevisiae.', 'Some Hypothesis', 'http://www.jcb.org/cgi/content/abstract/167/6/1099', 'user1', current_date, current_date, current_date);
