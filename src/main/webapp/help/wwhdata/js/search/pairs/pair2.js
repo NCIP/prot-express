577244e2592f6083deb83e2a69961db3554d68a0
klingerc <klingerc@mail.nih.gov>
2008-04-08 00:56:43 +0000

    SVN-Revision: 505

---
+++ src/main/webapp/help/wwhdata/js/search/pairs/pair2.js
@@ -0,0 +1,23 @@
+function FileData_Pairs(x)
+{
+x.t("searching","protexpress");
+x.t("managing","experiments");
+x.t("experiments","importing");
+x.t("working","protexpress");
+x.t("protexpress","working");
+x.t("protexpress","creating");
+x.t("protexpress","section");
+x.t("getting","started");
+x.t("new","experiment");
+x.t("includes","following");
+x.t("creating","new");
+x.t("started","searching");
+x.t("importing","data");
+x.t("section","includes");
+x.t("exporting","experiment");
+x.t("experiment","managing");
+x.t("experiment","data");
+x.t("data","exporting");
+x.t("topics","getting");
+x.t("following","topics");
+}
---
+++ src/main/webapp/help/wwhdata/js/search/pairs/pair9.js
@@ -0,0 +1,21 @@
+function FileData_Pairs(x)
+{
+x.t("protocols","managing");
+x.t("protocols","working");
+x.t("protocols","section");
+x.t("managing","protocols");
+x.t("deleting","protocol");
+x.t("working","protexpress");
+x.t("protexpress","creating");
+x.t("editing","protocol");
+x.t("new","experiment");
+x.t("includes","following");
+x.t("creating","new");
+x.t("section","includes");
+x.t("experiment","managing");
+x.t("topics","adding");
+x.t("protocol","deleting");
+x.t("protocol","editing");
+x.t("following","topics");
+x.t("adding","protocol");
+}
