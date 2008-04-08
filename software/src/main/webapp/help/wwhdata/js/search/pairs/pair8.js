b5403470a806c41077d447bedd1a079674ba1e54
klingerc <klingerc@mail.nih.gov>
2008-04-08 00:56:43 +0000

    SVN-Revision: 505

---
+++ software/src/main/webapp/help/wwhdata/js/search/pairs/pair8.js
@@ -0,0 +1,9 @@
+function FileData_Pairs(x)
+{
+x.t("working","protexpress");
+x.t("protexpress","creating");
+x.t("new","experiment");
+x.t("creating","new");
+x.t("experiment","working");
+x.t("experiment","creating");
+}
