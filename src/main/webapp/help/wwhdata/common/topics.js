d0a9942b2729c22f20dcecb9112332a1b103d386
klingerc <klingerc@mail.nih.gov>
2008-08-19 03:09:14 +0000

    SVN-Revision: 632

---
+++ src/main/webapp/help/wwhdata/common/topics.js
@@ -0,0 +1,18 @@
+function  WWHBookData_MatchTopic(P)
+{
+var C=null;
+if(P=="welcome_help")C="WelcometoprotExpressHelp.1.1.html#1091268";
+if(P=="search_help")C="WorkingWithprotExpress.2.6.html#1110016";
+if(P=="identify_experiment_help")C="WorkingWithprotExpress.2.8.html#1114612";
+if(P=="protocol_help")C="WorkingWithprotExpress.2.11.html#1112195";
+if(P=="add_protocol_help")C="WorkingWithprotExpress.2.12.html#1071260";
+if(P=="experiment_help")C="WorkingWithprotExpress.2.15.html#1088523";
+if(P=="add_experiment_help")C="WorkingWithprotExpress.2.17.html#1071285";
+if(P=="add_protocol_action_help")C="WorkingWithprotExpress.2.21.html#1099059";
+if(P=="add_protocol_application_input_help")C="WorkingWithprotExpress.2.24.html#1085749";
+if(P=="experiment_run_help")C="WorkingWithprotExpress.2.27.html#1084756";
+if(P=="add_experiment_run_help")C="WorkingWithprotExpress.2.28.html#1086273";
+if(P=="import_help")C="WorkingWithprotExpress.2.31.html#1085178";
+if(P=="export_help")C="WorkingWithprotExpress.2.32.html#1071326";
+return C;
+}
