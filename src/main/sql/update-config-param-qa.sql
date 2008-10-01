469a102ca28ba01d191de0ee86d235550d8bf5a0
kanchink <kanchink>
2008-10-01 04:38:25 +0000

    Updates to the xar generation algorithm. Updated sql scripts
    SVN-Revision: 691

--- src/main/sql/update-config-param-qa.sql
+++ src/main/sql/update-config-param-qa.sql
@@ -1,4 +1,5 @@
 update config_parameter set raw_value = 'true' where param = 'SEND_CONFIRM_EMAIL';
 update config_parameter set raw_value = 'false' where param = 'SEND_ADMIN_EMAIL';
 update config_parameter set raw_value = 'true' where param = 'DEVELOPMENT_MODE';
-update config_parameter set raw_value='NCINCICBGpsXarQATeam@mail.nih.gov' where param='REG_EMAIL_TO';
+update config_parameter set raw_value = 'NCINCICBGpsXarQATeam@mail.nih.gov' where param = 'REG_EMAIL_TO';
+
