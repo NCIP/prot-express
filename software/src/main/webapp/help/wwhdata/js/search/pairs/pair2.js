ce0bd7d647f7ed10a59358b95385fef2a45169bb
klingerc <klingerc@mail.nih.gov>
2008-10-09 20:11:09 +0000

    added welcome_home
    SVN-Revision: 720

---
+++ software/src/main/webapp/help/wwhdata/js/search/pairs/pair2.js
@@ -0,0 +1,23 @@
+function FileData_Pairs(x)
+{
+x.t("searching","protexpress");
+x.t("protocols","managing");
+x.t("managing","protocols");
+x.t("managing","experiments");
+x.t("experiments","exporting");
+x.t("working","protexpress");
+x.t("protexpress","working");
+x.t("protexpress","creating");
+x.t("protexpress","section");
+x.t("getting","started");
+x.t("new","experiment");
+x.t("includes","following");
+x.t("creating","new");
+x.t("started","searching");
+x.t("section","includes");
+x.t("exporting","experiment");
+x.t("experiment","managing");
+x.t("experiment","data");
+x.t("topics","getting");
+x.t("following","topics");
+}
