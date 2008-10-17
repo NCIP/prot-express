7fec13f44ee61f6f94545368bc7aabd4562f2a53
klingerc <klingerc@mail.nih.gov>
2008-10-17 02:38:01 +0000

    Updated help files
    SVN-Revision: 748

---
+++ software/src/main/webapp/help/wwhdata/js/search/pairs/pair9.js
@@ -0,0 +1,10 @@
+function FileData_Pairs(x)
+{
+x.t("working","protexpress");
+x.t("protexpress","viewing");
+x.t("details","working");
+x.t("details","text");
+x.t("details","viewing");
+x.t("viewing","experiment");
+x.t("experiment","details");
+}
