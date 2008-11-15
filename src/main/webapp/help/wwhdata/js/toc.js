0727bce825ca73b6c2ed841d952e78f33471792d
klingerc <klingerc@mail.nih.gov>
2008-11-15 04:27:18 +0000

    SVN-Revision: 807

---
+++ src/main/webapp/help/wwhdata/js/toc.js
@@ -0,0 +1,33 @@
+function  WWHBookData_AddTOCEntries(P)
+{
+var A=P.fN("Welcome to protExpress Online Help","0");
+var B=A.fN("Using protExpress Online Help","1");
+A=P.fN("Working with protExpress","2");
+B=A.fN("Getting Started","3");
+var C=B.fN("Registering Users","4");
+C=B.fN("Logging In","5");
+C=B.fN("Resetting Your Password","6");
+C=B.fN("Using the Home Page","7");
+B=A.fN("Searching protExpress","8");
+B=A.fN("Viewing Experiment Details","9");
+B=A.fN("Creating a New Experiment","10");
+C=B.fN("Identifying the Experiment","11");
+C=B.fN("Adding a Protocol to a New Experiment","12");
+C=B.fN("Adding an Existing Protocol to an Experiment","13");
+C=B.fN("Working with Inputs","14");
+var D=C.fN("Adding an Input","15");
+D=C.fN("Editing an Input","16");
+D=C.fN("Deleting an Input","17");
+C=B.fN("Working with Outputs","18");
+C=B.fN("Reviewing the Experiment","19");
+B=A.fN("Editing an Experiment","20");
+C=B.fN("Editing Experiment Details","21");
+C=B.fN("Editing Protocol Details","22");
+C=B.fN("Adding a Protocol to an Existing Experiment Run","23");
+C=B.fN("Working with Experiment Runs","24");
+D=C.fN("Adding an Experiment Run","25");
+D=C.fN("Editing an Experiment Run","26");
+D=C.fN("Deleting an Experiment Run","27");
+A=P.fN("protExpress Glossary","28");
+A=P.fN("Using Application Support","29");
+}
