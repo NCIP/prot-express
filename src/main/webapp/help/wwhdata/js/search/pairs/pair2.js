1778091f1cdd21dc38b1522982218ec7ca46a450
klingerc <klingerc@mail.nih.gov>
2008-11-16 06:38:41 +0000

    SVN-Revision: 810

---
+++ src/main/webapp/help/wwhdata/js/search/pairs/pair2.js
@@ -0,0 +1,21 @@
+function FileData_Pairs(x)
+{
+x.t("searching","protexpress");
+x.t("working","protexpress");
+x.t("protexpress","working");
+x.t("protexpress","viewing");
+x.t("protexpress","section");
+x.t("editing","experiment");
+x.t("getting","started");
+x.t("details","creating");
+x.t("new","experiment");
+x.t("includes","following");
+x.t("creating","new");
+x.t("started","searching");
+x.t("viewing","experiment");
+x.t("section","includes");
+x.t("experiment","editing");
+x.t("experiment","details");
+x.t("topics","getting");
+x.t("following","topics");
+}
---
+++ src/main/webapp/help/wwhdata/js/search/pairs/pair20.js
@@ -0,0 +1,10 @@
+function FileData_Pairs(x)
+{
+x.t("outputs","working");
+x.t("working","outputs");
+x.t("working","protexpress");
+x.t("protexpress","creating");
+x.t("new","experiment");
+x.t("creating","new");
+x.t("experiment","working");
+}
