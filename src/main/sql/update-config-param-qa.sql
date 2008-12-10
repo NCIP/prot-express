1c63db7567a0c5b984e2f58cd5c9641f21f36eeb
kanchink <kanchink>
2008-12-10 19:49:10 +0000

    updated the SYS_ADMIN_EMAIL param value.
    SVN-Revision: 892

--- src/main/sql/update-config-param-qa.sql
+++ src/main/sql/update-config-param-qa.sql
@@ -1,5 +1,5 @@
 update config_parameter set raw_value = 'true' where param = 'SEND_CONFIRM_EMAIL';
 update config_parameter set raw_value = 'true' where param = 'SEND_ADMIN_EMAIL';
 update config_parameter set raw_value = 'true' where param = 'DEVELOPMENT_MODE';
-update config_parameter set raw_value = 'NCINCICBGpsXarQATeam@mail.nih.gov' where param = 'REG_EMAIL_TO';
+update config_parameter set raw_value = 'NCINCICBGpsXarQATeam@mail.nih.gov' where param = 'SYS_ADMIN_EMAIL';
 
