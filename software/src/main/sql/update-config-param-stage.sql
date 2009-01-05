3bd7db39b87a682c7b575734e74abaaff4d8b4f9
kanchink <kanchink>
2009-01-05 17:45:20 +0000

    Updated the sys admin email id.
    SVN-Revision: 906

--- software/src/main/sql/update-config-param-stage.sql
+++ software/src/main/sql/update-config-param-stage.sql
@@ -1,2 +1,5 @@
-update config_parameter set raw_value = 'NCICB@pop.nci.nih.gov' where param = 'REG_EMAIL_TO';
-update config_parameter set raw_value = 'NCICB@pop.nci.nih.gov' where param = 'EMAIL_FROM';
+update config_parameter set raw_value = 'true' where param = 'SEND_CONFIRM_EMAIL';
+update config_parameter set raw_value = 'true' where param = 'SEND_ADMIN_EMAIL';
+update config_parameter set raw_value = 'false' where param = 'DEVELOPMENT_MODE';
+update config_parameter set raw_value = 'ncicb@pop.nci.nih.gov' where param = 'SYS_ADMIN_EMAIL';
+
